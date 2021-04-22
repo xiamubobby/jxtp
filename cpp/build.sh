9e42dfe5d0abc294d11d4e05cb5d17e86ff6d313
xiamubobby <xiamubobby@gmail.com>
2021-04-23 00:13:34 +0800

    Revert "Update build.sh"
    This reverts commit 937383bacf2cb9a26ab12bd49e13b864bf8a5c50.

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,5 +1,6 @@
 rm -rf build
 cmake CMakeLists.txt -B build
 cmake --build build --config Release
-lib_ext=$([[ "$(uname -s)" = "Darwin"  ]] && echo ".dylib" || echo ".so")
+lib_ext=".so"
+lib_ext=$([[ $(uname) = "Darwin" ]] && echo ".dylib")
 cp "build/lib/libjxtp${lib_ext}" ../lib
