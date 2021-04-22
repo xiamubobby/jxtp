18345a9cd45d9d01c8fedbae1bdc1354a609a858
xiamubobby <xiamubobby@gmail.com>
2021-04-23 00:13:34 +0800

    Revert "dynamic extension forging by telling Mac from Linux in build.sh"
    This reverts commit 27e9ff89a17ea7a3cec3026790a642ed179b0683.

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,6 +1,4 @@
 rm -rf build
 cmake CMakeLists.txt -B build
 cmake --build build --config Release
-lib_ext=".so"
-lib_ext=$([[ $(uname) = "Darwin" ]] && echo ".dylib")
-cp "build/lib/libjxtp${lib_ext}" ../lib
+cp build/lib/libjxtp.so ../lib
